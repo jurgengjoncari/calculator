e7a8bd97a3e6b4b1e7066a102aaac534d21c2e33
Jurgen n <jurgen.gjoncari@gmail.com>
2021-06-17 12:28:14 +0200

    Fix some things

--- pseudocode.md
+++ pseudocode.md
@@ -1,21 +1,30 @@
 There's a display and there's a numpad
 1. the user starts the program
-1. the display shows "0"
+1. x -> 0
+1. y -> ""
+1. operator -> ""
+1. the display shows x;
 1. the user clicks a button
 	- it is a digit
-		1. display the digit to the end
 		- has the operator been clicked yet?
 			- no
-				: save the digit to x
+				- is x = 0?
+					- yes
+						: replace x with that digit
+					- no
+						: add the digit to the end of x
 			- yes
-				: save the digit to y
-		
+				: add the digit to the end of y	
+	- it is an operator
+		1. operator -> button
+	- it is the "=" sign
+		1. call the operator function
+		1. display the result
+		1. x -> result
+		1. y -> ""
+		1. operator -> ""
 	- it is the "Clear" button
-		- is it only one character in display?
-			- no
-				1. delete the last character in display
-				1. remove the last character from any variable
-			- yes
-				1. 
-	- is it the "=" sign?
-	- is it an operator?
+		1. display "0"
+		1. x -> "0"
+		1. y -> ""
+		1. operator -> ""
--- script.js
+++ script.js
@@ -3,46 +3,55 @@ function substract(x, y) {return x - y;}
 function multiply(x, y) {return x * y;}
 function divide(x, y) {return x / y;}
 
-function operate(x, operator, y) {operator(x, y);}
-
-var input = 0;
-
-var x = new String;
-var operator = undefined;
-var y = new String;
-
-var buttons = document.querySelectorAll("button");
+function operate(x, operator, y) {
+	console.log(window[operator]);
+	return window[operator](x, y);
+}
 
 var outputDisplay = document.querySelector("#output");
 
+var x = "0";
+var operator = "";
+var y = new String;
+
 var operators = Array.from(document.querySelectorAll(".operator")).map((btn) => btn.id);
-console.log(operators);
-function display(input) {
+
+function display(button) {
 	if (outputDisplay.textContent == "0") {
 		outputDisplay.textContent = "";
 	}
-	if (input != "equals") {
-		outputDisplay.textContent += input;
-		if (operators.includes(input)) {
-			operator = input;
+	if (button.id == "clear") {
+		outputDisplay.textContent = "0";
+		x = "0";
+		y = "";
+		operator = "";
+	}
+	if (button.id != "equals") {
+		outputDisplay.textContent += button.textContent;
+		if (operators.includes(button.id)) {
+			operator = button.id;
 			console.log(operator);
 		} else {
-			if (operator == undefined) {
-				x.concat(input);
+			if (operator == "") {
+				x = x.concat(button.textContent);
 			} else {
-				y.concat(input);
+				y = y.concat(button.textContent);
 			}
 		}
 	} else {
-		console.log(operator);
-		outputDisplay.textContent = operate(Number(x), operator, Number(y));
+		result = operate(Number(x), operator, Number(y))
+		outputDisplay.textContent = result;
+		x = result;
+		y = "";
+		operator = "";
 	}
 }
 
+var buttons = document.querySelectorAll("button");
+
 buttons.forEach(function (button) {
 	button.addEventListener("click", function () {
-		input = button.id;
-		display(input);
+		display(button);
 	})
 });
 
