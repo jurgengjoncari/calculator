536113416774847a1e34ea21bccd2ca5884c5d8a
Jurgen <jurgen_gjoncari@icloud.com>
2022-03-23 23:24:19 +0100

    Finish refactor of logic

--- script.js
+++ script.js
@@ -1,92 +1,102 @@
-const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-const operators = ['+', '-', '*', '/']
-
-const digitButtons = document.querySelectorAll('[data-digit]')
-const operatorButtons = document.querySelectorAll('[data-operator]')
-const equalsButton = document.querySelector('[data-equals]')
-
 class Calculator {
     constructor() {
         this.operator = undefined
-        this.currentOperand = ''
-        this.previewsOperand = '0'
+        this.currentOperand = '0'
+        this.previewsOperand = ''
+        this.output = document.querySelector('[data-output]')
     }
-    
-    output = document.querySelector('[data-output]')
 
     clear() {
         this.operator = undefined
-        this.currentOperand = ''
-        this.previewsOperand = '0'
+        this.currentOperand = '0'
+        this.previewsOperand = ''
+        this.output.textContent = '0'
     }
 
-    appendNumber(number) {
-        console.log(this.currentOperand);
-        if (this.operator == undefined) {
-            if (this.previewsOperand == '0') {
-                this.previewsOperand = number
-            } else {
-                this.previewsOperand = String(this.previewsOperand).concat(number)
-            }
+    appendDigit(digit) {
+        if (this.currentOperand == '0') {
+            this.currentOperand = digit
         } else {
-            this.currentOperand = String(this.currentOperand).concat(number)
+            this.currentOperand = String(this.currentOperand).concat(digit)
         }
     }
-    
-    calculate(operator) {
+
+    appendDecimal() {
+        if (this.currentOperand % 1 == 0) {
+            this.currentOperand = String(this.currentOperand).concat('.')
+        }
         if (this.currentOperand == '') {
-            this.currentOperand = this.previewsOperand
+            this.currentOperand = '0.'
         }
-        if (this.operator == "+") {
-            this.previewsOperand = Number(this.previewsOperand) + Number(this.currentOperand)
-        } else if (this.operator == "-") {
-            this.previewsOperand -= this.currentOperand
-        } else if (operator == "*") {
-            this.previewsOperand *= this.currentOperand
-        } else if (operator == "/") {
-            this.previewsOperand /= this.currentOperand
-        }
-        this.currentOperand = ''
-        return this.previewsOperand
-    }
-    
-    updateDisplay() {
-        this.output.textContent = this.previewsOperand
+        return this.currentOperand
     }
     
     setOperator(operator) {
-        if (this.operator != undefined && operand != '') {
-            this.previewsOperand = calculate(operator)
-        }
         this.operator = operator
+        this.previewsOperand = this.currentOperand
+        this.currentOperand = ''
     }
     
     compute() {
-        if (this.operator != undefined) {
-            this.previewsOperand = calculate(this.operator)
+        if (this.currentOperand == '') {
+            this.currentOperand = this.previewsOperand
         }
-        this.operator = undefined
-    }   
+        let result
+        const x = parseFloat(this.previewsOperand)
+        const y = parseFloat(this.currentOperand)
+        switch (this.operator) {
+            case '+':
+                result = x + y
+                break
+            case '-':
+                result = x - y
+                break
+            case '*':
+                result = x * y
+                break
+            case '/':
+                result = x / y
+                break
+        }
+        this.currentOperand = result
+        return result
+    }
 }
 
 const calculator = new Calculator
 
-const calculatorGrid = document.querySelector('#calculator')
+const digitButtons = document.querySelectorAll('[data-digit]')
+const operatorButtons = document.querySelectorAll('[data-operator]')
+const equalsButton = document.querySelector('[data-equals]')
+const clearButton = document.querySelector('[data-clear]')
+const decimalButton = document.querySelector('[data-decimal]')
+
+const output = document.querySelector('[data-output]')
 
 digitButtons.forEach((button) => {
     button.addEventListener('click', () => {
-        calculator.appendNumber(button.textContent)
-        calculator.updateDisplay()
+        calculator.appendDigit(button.textContent)
+        output.textContent = button.textContent
     })
+})
+
+decimalButton.addEventListener('click', () => {
+    output.textContent = calculator.appendDecimal()
 })
 
 operatorButtons.forEach((button) => {
     button.addEventListener('click', () => {
+        if (calculator.operator != undefined && calculator.currentOperand != '') {
+            output.textContent = calculator.compute(button.textContent)
+        }
         calculator.setOperator(button.textContent)
-        calculator.updateDisplay()
     })
 })
 
 equalsButton.addEventListener('click', () => {
-    calculator.compute()
+    output.textContent = calculator.compute()
+})
+
+clearButton.addEventListener('click', () => {
+    calculator.clear()
 })
